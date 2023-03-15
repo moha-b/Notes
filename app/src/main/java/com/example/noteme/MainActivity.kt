4d33f42dd52666128fbb1517efb1ab5eb943648d
hab <Mohabzax4@gmail.com>
2023-03-15 18:42:08 +0200

    Navigation Graph

--- app/src/main/java/com/example/noteme/MainActivity.kt
+++ app/src/main/java/com/example/noteme/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.noteme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
